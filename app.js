60278cf26c9735f3599776ee7acb6a17a75f2a23
Phumin Tongla <110321268+Natsu2022@users.noreply.github.com>
2024-03-31 17:13:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 app.use(bodyParser.json());
 const corsOptions = {
-    origin: 'http://localhost:5173/',
+    origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST']
 };
